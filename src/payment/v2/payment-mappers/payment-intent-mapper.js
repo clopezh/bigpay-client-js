b4778b319be69154e5104f94c857af3291cbee48
Carlos Lopez <clopez@intersysconsulting.com>
2019-06-19 11:51:42 -0500

    feat(payments): stripe-Vault Add support for new endpoint to create Payment Intents

---
+++ src/payment/v2/payment-mappers/payment-intent-mapper.js
@@ -0,0 +1,73 @@
+import { omitNil } from '../../../common/utils';
+import GatewayMapper from './gateway-mapper';
+import QuoteMapper from './quote-mapper';
+import StoreMapper from './store-mapper';
+
+export default class PaymentIntentMapper {
+    /**
+     * @returns {PaymentIntentMapper}
+     */
+    static create() {
+        const gatewayMapper = GatewayMapper.create();
+        const quoteMapper = QuoteMapper.create();
+        const storeMapper = StoreMapper.create();
+
+        return new PaymentIntentMapper(gatewayMapper, quoteMapper, storeMapper);
+    }
+
+    /**
+     * @param {GatewayMapper} gatewayMapper
+     * @param {QuoteMapper} quoteMapper
+     * @param {StoreMapper} storeMapper
+     */
+    constructor(gatewayMapper, quoteMapper, storeMapper) {
+        /**
+         * @private
+         * @type {GatewayMapper}
+         */
+        this.gatewayMapper = gatewayMapper;
+
+        /**
+         * @private
+         * @type {QuoteMapper}
+         */
+        this.quoteMapper = quoteMapper;
+
+        /**
+         * @private
+         * @type {StoreMapper}
+         */
+        this.storeMapper = storeMapper;
+    }
+
+    /**
+     * @param {PaymentRequestData} data
+     * @returns {Object}
+     */
+    mapToPaymentIntent(data) {
+        return omitNil({
+            stripe_data: this.mapStripeData(data),
+            gateway: this.gatewayMapper.mapToGateway(data),
+        });
+    }
+
+    mapStripeData(data) {
+        const {
+            shouldSavePaymentInstrument = false,
+            customer = {},
+            cart = {},
+            store = {},
+        } = data;
+
+        return omitNil({
+            should_save_payment_instrument: shouldSavePaymentInstrument,
+            customer_id: customer.customerId,
+            customer_name: customer.name,
+            customer_email: customer.email,
+            grand_total: cart.grandTotal.integerAmount,
+            currency_code: cart.currency,
+            store_id: store.storeId,
+            store_name: store.storeName,
+        });
+    }
+}
